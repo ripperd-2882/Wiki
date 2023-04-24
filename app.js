d63ad1812d4d66038535d342a0e20bcb3f973e8a
ripperd-2882 <ripperd2882@gmail.com>
2023-04-24 23:58:35 +0530

    Chained Route Handlers

--- app.js
+++ app.js
@@ -1,59 +1,60 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://127.0.0.1:27017/wikiDB");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true,}));
 app.use(express.static("public"));
 
 const articleSchema = {
-    name: String,
-    content: String
+  name: String,
+  content: String,
 };
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function (req, res) {
-    Article.find().then(function (foundArticles) {
+app.route("/articles")
+
+  .get(function (req, res) {
+    Article.find()
+      .then(function (foundArticles) {
         res.send(foundArticles);
-    })
-        .catch(function (err) {
-            res.send(err)
-        })
-})
+      })
+      .catch(function (err) {
+        res.send(err);
+      });
+  })
 
-app.post("/articles",function (req,res) {   
-
-    const newArticle=new Article({
-        title:req.body.title,
-        content:req.body.content
+  .post(function (req, res) {
+    const newArticle = new Article({
+      name: req.body.name,
+      content: req.body.content,
     });
 
-    newArticle.save().then(function (){
+    newArticle.save().then(function () {
         res.send("Added successfully");
-    })
-    .catch(function (err) {
-        res.send(err);        
-    });
-})
+      })
+      .catch(function (err) {
+        res.send(err);
+      });
+  })
 
-app.delete("/articles",function (req,res) {
-    Article.deleteMany().then(function(){
-        res.send("Deleted Successfully")
-    })    
-    .catch(function(err){
-        res.send(err)
-    })
-})
+  .delete(function (req, res) {
+    Article.deleteMany()
+      .then(function () {
+        res.send("Deleted Successfully");
+      })
+      .catch(function (err) {
+        res.send(err);
+      });
+  });
 
 app.listen(3000, function () {
-    console.log("Server started on port 3000");
+  console.log("Server started on port 3000");
 });
